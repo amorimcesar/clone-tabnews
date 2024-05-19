7d0c1749541d90f5e1958ce53e4392710ec8c855
Cesar Amorim <cesar.amorim49@gmail.com>
2024-05-19 16:30:36 -0300

    finish `/api/vi/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseInfo = await database.query(
+    `SELECT
+    current_setting('server_version') AS server_version,
+    current_setting('max_connections')::int AS max_connections,
+    (SELECT count(*)::int FROM pg_stat_activity WHERE state = 'active') AS current_connections;
+    `,
+  );
+
+  const databaseVersionValue = databaseInfo.rows[0].server_version;
+  const databaseMaxConnections = databaseInfo.rows[0].max_connections;
+  const databaseOpenedConnectionsValue =
+    databaseInfo.rows[0].current_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: databaseMaxConnections,
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
