b40d4a02f4c6de64b3e3011d092002ebf2464c67
César Amorim <cesar.amorim49@gmail.com>
2025-11-06 00:16:58 -0300

    feat: create and use InternalServerError in /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,26 +1,36 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const databaseName = process.env.POSTGRES_DB;
-  const updatedAt = new Date().toISOString();
-  const result = await database.query({
-    text: `
+  try {
+    const databaseName = process.env.POSTGRES_DB;
+    const updatedAt = new Date().toISOString();
+    const result = await database.query({
+      text: `
     SELECT
       current_setting('server_version') AS version,
       current_setting('max_connections')::int AS max_connections,
       (SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1) AS used_connections;
   `,
-    values: [databaseName],
-  });
-  const dbInfo = result.rows[0];
-  response.status(200).json({
-    updated_at: updatedAt,
-    database: {
-      version: dbInfo.version,
-      max_connections: dbInfo.max_connections,
-      used_connections: parseInt(dbInfo.used_connections, 10),
-    },
-  });
-}
+      values: [databaseName],
+    });
+    const dbInfo = result.rows[0];
+    response.status(200).json({
+      updated_at: updatedAt,
+      database: {
+        version: dbInfo.version,
+        max_connections: dbInfo.max_connections,
+        used_connections: parseInt(dbInfo.used_connections, 10),
+      },
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
 
+    console.log(`\n Erro dentro do catch do controller:`);
+    console.log(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
+}
 export default status;
