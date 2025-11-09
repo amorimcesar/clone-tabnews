0eeeaa83ed5db599306fd521eb1724625760fd7b
César Amorim <cesar.amorim49@gmail.com>
2025-11-09 10:36:08 -0300

    feat: create migrator model

---
+++ models/migrator.js
@@ -0,0 +1,65 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+
+function getDefaultMigrationOptions(dbClient, dryRun) {
+  return {
+    dbClient: dbClient,
+    dryRun: dryRun,
+    dir: resolve(process.cwd(), "infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+}
+
+async function listPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrationOptions = {
+      ...getDefaultMigrationOptions(dbClient, true),
+    };
+
+    const pendingMigrations = await migrationRunner(migrationOptions);
+    return pendingMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: "Não foi possível listar as migrações pendentes.",
+      cause: error,
+    });
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function runPendingMigrations() {
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migrationOptions = {
+      ...getDefaultMigrationOptions(dbClient, false),
+    };
+
+    const executedMigrations = await migrationRunner(migrationOptions);
+    return executedMigrations;
+  } catch (error) {
+    throw new ServiceError({
+      message: "Não foi possível executar as migrações pendentes.",
+      cause: error,
+    });
+  } finally {
+    await dbClient.end();
+  }
+}
+
+const migrator = {
+  listPendingMigrations,
+  runPendingMigrations,
+};
+
+export default migrator;
